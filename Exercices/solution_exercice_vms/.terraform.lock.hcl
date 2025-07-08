20b66e3e3d2384758a125b7d459f96404d0eed0b
ihab abadi <ihab@utopios.net>
2025-07-08 10:43:09 +0100

    solution exercice vm

---
+++ Exercices/solution_exercice_vms/.terraform.lock.hcl
@@ -0,0 +1,64 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/local" {
+  version     = "2.4.1"
+  constraints = "~> 2.4.0"
+  hashes = [
+    "h1:gpp25uNkYJYzJVnkyRr7RIBVfwLs9GSq2HNnFpTRBg0=",
+    "zh:244b445bf34ddbd167731cc6c6b95bbed231dc4493f8cc34bd6850cfe1f78528",
+    "zh:3c330bdb626123228a0d1b1daa6c741b4d5d484ab1c7ae5d2f48d4c9885cc5e9",
+    "zh:5ff5f9b791ddd7557e815449173f2db38d338e674d2d91800ac6e6d808de1d1d",
+    "zh:70206147104f4bf26ae67d730c995772f85bf23e28c2c2e7612c74f4dae3c46f",
+    "zh:75029676993accd6bef933c196b2fad51a9ec8a69a847dbbe96ec8ebf7926cdc",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:7d48d5999fe1fcdae9295a7c3448ac1541f5a24c474bd82df6d4fa3732483f2b",
+    "zh:b766b38b027f0f84028244d1c2f990431a37d4fc3ac645962924554016507e77",
+    "zh:bfc7ad301dada204cf51c59d8bd6a9a87de5fddb42190b4d6ba157d6e08a1f10",
+    "zh:c902b527702a8c5e2c25a6637d07bbb1690cb6c1e63917a5f6dc460efd18d43f",
+    "zh:d68ae0e1070cf429c46586bc87580c3ed113f76241da2b6e4f1a8348126b3c46",
+    "zh:f4903fd89f7c92a346ae9e666c2d0b6884c4474ae109e9b4bd15e7efaa4bfc29",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version     = "4.0.6"
+  constraints = "~> 4.0.0"
+  hashes = [
+    "h1:n3M50qfWfRSpQV9Pwcvuse03pEizqrmYEryxKky4so4=",
+    "zh:10de0d8af02f2e578101688fd334da3849f56ea91b0d9bd5b1f7a243417fdda8",
+    "zh:37fc01f8b2bc9d5b055dc3e78bfd1beb7c42cfb776a4c81106e19c8911366297",
+    "zh:4578ca03d1dd0b7f572d96bd03f744be24c726bfd282173d54b100fd221608bb",
+    "zh:6c475491d1250050765a91a493ef330adc24689e8837a0f07da5a0e1269e11c1",
+    "zh:81bde94d53cdababa5b376bbc6947668be4c45ab655de7aa2e8e4736dfd52509",
+    "zh:abdce260840b7b050c4e401d4f75c7a199fafe58a8b213947a258f75ac18b3e8",
+    "zh:b754cebfc5184873840f16a642a7c9ef78c34dc246a8ae29e056c79939963c7a",
+    "zh:c928b66086078f9917aef0eec15982f2e337914c5c4dbc31dd4741403db7eb18",
+    "zh:cded27bee5f24de6f2ee0cfd1df46a7f88e84aaffc2ecbf3ff7094160f193d50",
+    "zh:d65eb3867e8f69aaf1b8bb53bd637c99c6b649ba3db16ded50fa9a01076d1a27",
+    "zh:ecb0c8b528c7a619fa71852bb3fb5c151d47576c5aab2bf3af4db52588722eeb",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/terraform-provider-openstack/openstack" {
+  version     = "1.54.1"
+  constraints = "~> 1.54.0"
+  hashes = [
+    "h1:Cqk18+r4bJF/sIusEK9lM0gc841RwsJ8AMhWyiU7lig=",
+    "zh:45ba84df17f94b15af7aab7007241e035dde8a5b46aeb761259d937058a80f71",
+    "zh:493b1deb7be9b600e5b1f5da2a9dfd3bce5df0c6d38090614dbe4ed05ade8441",
+    "zh:53551401fba8c1d5b27a08ee307552b84b1d0c1218f3717a4b766ec701b3e016",
+    "zh:53629bebb48ce5220f7601d776c2ac1485b6c860cb695f150fb716f5be8aa86d",
+    "zh:5a20f32cca767bef70b79bc8ecbd10fec3dc8696183e2d29631aa510947cb70d",
+    "zh:653693f630777e4aa3f410976a5169cf0f2a301516a820b3860de116054ae30a",
+    "zh:70f2d7bd5f5940f4fc3f023a01468890fbd9d704d0256bc65f7c64fb2cbcd4e4",
+    "zh:9cc22af51e5124dd5c2e0f1adefb1b08dcff3138aba9c92961cef36b1641d7aa",
+    "zh:9df45e893f215266159733dbc120809bc3d313188e121532dc6e2d10165e9899",
+    "zh:cb3e240992069cd6160f5b5cbbd50b70948f25bb337a75e780a0648461505d3f",
+    "zh:cb8343c0cf1bf5ca4d060826a8b68e3e5935b4a65974c76ac9c071c5a510e67e",
+    "zh:cc2060f93c66276dff6366b48e3a0e619874e3d939e0d2a39fc6ce10ca91232d",
+    "zh:d495b3051977018696113eded89c2cddfae0570f2adbdf7e9097c189ba41903e",
+    "zh:dfad1be943769780d5e948c06db957ce45f98b057a774964da0b82130c22f139",
+  ]
+}
--- Exercices/solution_exercice_vms/main.tf
+++ Exercices/solution_exercice_vms/main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_version = ">= 1.0"
   required_providers {
@@ -17,15 +16,11 @@ terraform {
   }
 }
 
-provider "local" {
-  
+provider "openstack" {
+    
 }
-
-provider "tls" {
-  
-}
-
-provider "openstack" {}
+provider "tls" {}
+provider "local" {}
 
 variable "project_name" {
   default = "terraform-lab"
@@ -39,6 +34,31 @@ variable "volume_size" {
   default = 20
 }
 
+resource "openstack_images_image_v2" "ubuntu_image" {
+  name             = "${var.project_name}-ubuntu-22-04"
+  image_source_url = "https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  #web_download     = true
+  verify_checksum  = true
+  
+  properties = {
+    os_type    = "linux"
+    os_distro  = "ubuntu"
+    os_version = "22.04"
+  }
+}
+
+resource "openstack_compute_flavor_v2" "custom_flavor" {
+  name  = "${var.project_name}-flavor"
+  ram   = "2048"
+  vcpus = "2"
+  disk  = "20"
+  
+  extra_specs = {
+    "hw:cpu_policy" = "shared"
+  }
+}
 
 resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
@@ -56,52 +76,173 @@ resource "local_file" "public_key" {
   filename = "${var.project_name}-key.pub"
 }
 
-
-resource "openstack_images_image_v2" "ubuntu_image" {
-}
-
-resource "openstack_compute_flavor_v2" "custom_flavor" {
-
-}
-
-
 resource "openstack_compute_keypair_v2" "ssh_keypair" {
-
+  name       = "${var.project_name}-keypair"
+  public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
-data "openstack_networking_network_v2" "external_network" {
+# data "openstack_networking_network_v2" "external_network" {
+#   name     = "external"
+#   external = true
+# }
 
+resource "openstack_networking_network_v2" "private_network" {
+  name           = "${var.project_name}-network"
+  admin_state_up = true
 }
 
-resource "openstack_networking_network_v2" "private_network"  {
-
-}
 resource "openstack_networking_subnet_v2" "private_subnet" {
-
+  name       = "${var.project_name}-subnet"
+  network_id = openstack_networking_network_v2.private_network.id
+  cidr       = "10.0.1.0/24"
+  ip_version = 4
+  dns_nameservers = ["8.8.8.8", "8.8.4.4"]
+  
+  allocation_pool {
+    start = "10.0.1.10"
+    end   = "10.0.1.200"
+  }
 }
 
-resource "openstack_networking_router_v2" "router" {
+# resource "openstack_networking_router_v2" "router" {
+#   name                = "${var.project_name}-router"
+#   admin_state_up      = true
+#   external_network_id = data.openstack_networking_network_v2.external_network.id
+# }
 
-}
-
-resource "openstack_networking_router_interface_v2" "router_interface"  {
-
-}
+# resource "openstack_networking_router_interface_v2" "router_interface" {
+#   router_id = openstack_networking_router_v2.router.id
+#   subnet_id = openstack_networking_subnet_v2.private_subnet.id
+# }
 
 resource "openstack_networking_secgroup_v2" "secgroup" {
-
+  name        = "${var.project_name}-secgroup"
+  description = "Security group for VMs"
 }
-resource "openstack_networking_secgroup_rule_v2" "ssh_rule" {
 
+resource "openstack_networking_secgroup_rule_v2" "ssh_rule" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "http_rule" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "https_rule" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "icmp_rule" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
 }
 
 resource "openstack_blockstorage_volume_v3" "volumes" {
+  count = var.vm_count
+  
+  name = "${var.project_name}-volume-${count.index + 1}"
+  size = var.volume_size
 }
 
-resource "openstack_compute_instance_v2" "vms" {
+# resource "openstack_networking_floatingip_v2" "floating_ips" {
+#   count = var.vm_count
+  
+#   pool = "external"
+# }
 
+resource "openstack_compute_instance_v2" "vms" {
+  count = var.vm_count
+  
+  name            = "${var.project_name}-vm-${count.index + 1}"
+  image_id        = openstack_images_image_v2.ubuntu_image.id
+  flavor_id       = openstack_compute_flavor_v2.custom_flavor.id
+  key_pair        = openstack_compute_keypair_v2.ssh_keypair.name
+  security_groups = [openstack_networking_secgroup_v2.secgroup.name]
+  
+  network {
+    uuid = openstack_networking_network_v2.private_network.id
+  }
+  
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y nginx htop curl
+    systemctl start nginx
+    systemctl enable nginx
+    echo "<h1>VM ${count.index + 1} - ${var.project_name}</h1>" > /var/www/html/index.html
+    echo "<p>Hostname: $(hostname)</p>" >> /var/www/html/index.html
+    echo "<p>IP: $(hostname -I)</p>" >> /var/www/html/index.html
+    echo "<p>Date: $(date)</p>" >> /var/www/html/index.html
+  EOF
+  )
+  
+  depends_on = [
+    openstack_networking_subnet_v2.private_subnet,
+    #openstack_networking_router_interface_v2.router_interface
+  ]
 }
 
 resource "openstack_compute_volume_attach_v2" "volume_attachments" {
-    
+  count = var.vm_count
+  
+  instance_id = openstack_compute_instance_v2.vms[count.index].id
+  volume_id   = openstack_blockstorage_volume_v3.volumes[count.index].id
+}
+
+# resource "openstack_compute_floatingip_associate_v2" "floating_ip_associations" {
+#   count = var.vm_count
+  
+#   floating_ip = openstack_networking_floatingip_v2.floating_ips[count.index].address
+#   instance_id = openstack_compute_instance_v2.vms[count.index].id
+# }
+
+output "vm_info" {
+  value = {
+    for i in range(var.vm_count) : "vm-${i + 1}" => {
+      name        = openstack_compute_instance_v2.vms[i].name
+      private_ip  = openstack_compute_instance_v2.vms[i].network[0].fixed_ip_v4
+      #floating_ip = openstack_networking_floatingip_v2.floating_ips[i].address
+      volume_id   = openstack_blockstorage_volume_v3.volumes[i].id
+      #ssh_command = "ssh -i ${var.project_name}-key.pem ubuntu@${openstack_networking_floatingip_v2.floating_ips[i].address}"
+    }
+  }
+}
+
+output "network_info" {
+  value = {
+    network_name = openstack_networking_network_v2.private_network.name
+    subnet_cidr  = openstack_networking_subnet_v2.private_subnet.cidr
+    #router_name  = openstack_networking_router_v2.router.name
+  }
+}
+
+output "created_resources" {
+  value = {
+    image_name  = openstack_images_image_v2.ubuntu_image.name
+    flavor_name = openstack_compute_flavor_v2.custom_flavor.name
+    keypair     = openstack_compute_keypair_v2.ssh_keypair.name
+    network     = openstack_networking_network_v2.private_network.name
+    secgroup    = openstack_networking_secgroup_v2.secgroup.name
+  }
 }
